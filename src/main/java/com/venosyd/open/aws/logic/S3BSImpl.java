e429246ffb604b33c6f5ab8717cf63cf08772c6c
Sergio Lisan <sels@venosyd.com>
2020-12-02 03:29:43 -0300

    updates

--- src/main/java/com/venosyd/open/aws/logic/S3BSImpl.java
+++ src/main/java/com/venosyd/open/aws/logic/S3BSImpl.java
@@ -1,5 +1,7 @@
 package com.venosyd.open.aws.logic;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.util.Base64;
 
 import com.amazonaws.AmazonServiceException;
@@ -12,6 +14,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.venosyd.open.aws.lib.AWSUtil;
 import com.venosyd.open.commons.log.Debuggable;
+import com.venosyd.open.commons.util.SaveImage;
 
 /**
  * @author sergio lisan <sels@venosyd.com>
@@ -20,8 +23,11 @@ public class S3BSImpl implements S3BS, Debuggable {
 
     @Override
     public String upload(String bucket, String key, String file) {
+        // salva o arquivo como cache local
         byte[] data = (file.contains(",")) ? Base64.getDecoder().decode(file.substring(file.indexOf(",") + 1))
                 : Base64.getDecoder().decode(file);
+
+        SaveImage.saveImage("storage/", bucket + "_" + key, new ByteArrayInputStream(data));
 
         try {
             Regions clientRegion = Regions.fromName(AWSUtil.getValue("region"));
@@ -32,7 +38,11 @@ public class S3BSImpl implements S3BS, Debuggable {
             AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
                     .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(clientRegion).build();
 
-            s3Client.putObject(bucket, key, new String(data));
+            File arquivo = new File("storage/" + bucket + "_" + key);
+            s3Client.putObject(bucket, key, arquivo);
+
+            arquivo.delete(); // remove o cache local depois do upload
+
             return "SUCCESS";
         }
         //
